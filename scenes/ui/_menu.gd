f0fd495d9b60b9aa2bd9454e061d69df4d89f739
Nathãn Lima <nathan2001lima@gmail.com>
2025-01-20 17:05:18 -0300

    refactor: organiza os locais das cenas e scripts

---
+++ scenes/ui/_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+var timer
+
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_GO_BACK_REQUEST:
+		_on_Back_pressed()
+	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
+		_on_Back_pressed()
+
+func ready():
+	get_tree().set_quit_on_go_back(false)
+	timer = get_node("Timer")
+	timer.start()
+
+func _on_jogar_pressed():
+	get_tree().change_scene("res://scenes/levels/level_1.tscn")
+
+func _on_Back_pressed():
+	get_tree().quit()
