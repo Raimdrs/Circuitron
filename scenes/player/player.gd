6ef3eda79a9b3d594f582c8c51a641d328f93957
Nathãn Lima <nathan2001lima@gmail.com>
2025-01-20 16:09:17 -0300

    feat: adiciona imagens de espinhos e remove script actor

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,9 +1,13 @@
-class_name player
-extends actor
+class_name player extends KinematicBody2D
 
-const UP = Vector2(0,-1)
+export var speed = Vector2(150.0, 500.0)
+onready var gravity = ProjectSettings.get("physics/2d/default_gravity")
+
 const ACCELERATION = 30
+
 var can_pick = true
+var _velocity = Vector2.ZERO
+
 onready var animation_player = $animation
 
 func _physics_process(_delta):
@@ -26,21 +30,19 @@ func _physics_process(_delta):
 		if Input.is_action_just_pressed("ui_up"):
 			_velocity.y = -speed.y
 			$jump.play()
-		if friction == true:
+		if friction:
 			_velocity.x = lerp(_velocity.x , 0, 0.2)
 	else: 
 		if _velocity.y > 0:
 			anim_switch("fall")
 		else:
 			anim_switch("jump")
-		if friction == true:
+		if friction:
 			_velocity.x = lerp(_velocity.x , 0, 0.6)
-	_velocity = move_and_slide(_velocity, UP)
+	_velocity = move_and_slide(_velocity, Vector2.UP)
 
 
 func anim_switch(animation):
 	var new_anim = str(animation)
 	if animation_player.current_animation != new_anim:
 		animation_player.play(new_anim)
-
-
