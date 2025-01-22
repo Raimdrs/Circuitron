127d814eaf5acaa066ea0d3a02e0e471fe8432d8
Nathãn Lima <nathan2001lima@gmail.com>
2025-01-22 20:53:15 -0300

    feat: morte do personagem com reset da cena

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,10 +5,14 @@ class_name player extends CharacterBody2D
 @export var speed = Vector2(130.0, 500.0)
 @onready var gravity = ProjectSettings.get("physics/2d/default_gravity")
 
+var alive = true
 var can_pick = true
 var _velocity = Vector2.ZERO
 
 func _physics_process(delta):
+	if not alive:
+		return
+
 	if not is_on_floor():
 		_velocity.y += gravity * delta
 	
@@ -19,17 +23,20 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		_velocity.x = direction * speed.x
-		anim_switch("run")
+		animated_sprite.play("run")
 		animated_sprite.flip_h = direction == -1
 	else:
-		anim_switch("idle")
+		animated_sprite.play("idle")
 		_velocity.x = move_toward(_velocity.x, 0, speed.x)
 	set_velocity(_velocity)
 	set_up_direction(Vector2.UP)
 	move_and_slide()
 	_velocity = velocity
 
-func anim_switch(animation):
-	var new_anim = str(animation)
-	if animated_sprite.animation != new_anim:
-		animated_sprite.play(new_anim)
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite.animation == "death":
+		get_tree().reload_current_scene()
+
+func _on_damage_area_body_entered(_body):
+	animated_sprite.play("death")
+	alive = false
