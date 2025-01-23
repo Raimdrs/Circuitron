563f759fa7f7b7ea86237e80e2070abf581da1c7
Nathãn Lima <nathan2001lima@gmail.com>
2025-01-23 16:43:25 -0300

    feat: organiza o arquivo do PlayerGUI e cria GreenVirus

---
+++ scenes/green_virus/green_virus.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var speed := Vector2(50, 0)
+@onready var gravity = ProjectSettings.get("physics/2d/default_gravity")
+
+@onready var current_direction := Vector2.RIGHT
+
+func _process(delta):
+	$AnimatedSprite2D.flip_h = current_direction == Vector2.LEFT
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	velocity.x = (speed * current_direction).x;
+	
+	move_and_slide()
+
+
+func _on_wall_detector_body_entered(body):
+	if current_direction == Vector2.RIGHT:
+		current_direction = Vector2.LEFT
+	else:
+		current_direction = Vector2.RIGHT
