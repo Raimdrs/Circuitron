370d21e0a78216199a08b7e783a0896015fda21d
Raimdrs <raiimdrs@gmail.com>
2025-01-26 17:54:45 -0300

    add: level 3

--- scenes/checkpoint.gd
+++ scenes/checkpoint.gd
@@ -10,3 +10,7 @@ func _on_body_entered(_body):
 	if Globals.respawn_point != $RespawnPoint.global_position && $AnimatedSprite2D.animation != "new_touch":
 		$AnimatedSprite2D.play("new_touch")
 	entered_checkpoint.emit($RespawnPoint.global_position)
+
+
+func _on_entered_checkpoint() -> void:
+	pass # Replace with function body.
