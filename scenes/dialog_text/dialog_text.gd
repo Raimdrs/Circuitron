b187f579d9c6edcce2807aa377724697519622cc
Raimdrs <raiimdrs@gmail.com>
2025-01-28 16:55:07 -0300

    fix: Texturas Melhorada

--- scenes/dialog_text/dialog_text.gd
+++ scenes/dialog_text/dialog_text.gd
@@ -6,6 +6,6 @@ var react
 
 func _process(_delta):
 	react = get_node(previous).play
-	self.modulate.a8 = 150
+	self.modulate.a8 = 500
 	self.position = get_node("../player/Camera2D/dialog").global_position
 	self.visible = react
--- scenes/ui/_logo_final.gd
+++ scenes/ui/_logo_final.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_Timer_timeout():
-	get_tree().change_scene_to_file("res://scr/user_interface/credits.tscn")
+	get_tree().change_scene_to_file("res://scenes/ui/credits.tscn")
